746ec5f8e1d4b0279585b3284f8c7fe66b80a04f
Hyspan <32776601+krokman@users.noreply.github.com>
2019-05-12 03:35:00 +0300

    logg

--- src/main/java/util/HibernateSessionFactoryUtil.java
+++ src/main/java/util/HibernateSessionFactoryUtil.java
@@ -2,12 +2,14 @@ package util;
 
 import model.Good;
 import model.User;
+import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateSessionFactoryUtil {
 	private static SessionFactory sessionFactory;
+	final static Logger logger = Logger.getLogger(HibernateSessionFactoryUtil.class);
 
 	private HibernateSessionFactoryUtil() {
 	}
@@ -22,7 +24,7 @@ public class HibernateSessionFactoryUtil {
 				sessionFactory = configuration.buildSessionFactory(builder.build());
 
 			} catch (Exception e) {
-				System.out.println("Исключение!" + e);
+				logger.error("wrong session data", e);
 			}
 		}
 		return sessionFactory;
